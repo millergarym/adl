a9d4fc74482f9eda416a7c0e41e159cd4db67de5
Gary Miller <miller.garym@gmail.com>
2023-04-04 14:39:09 +1000

    working for protoapp

--- rust/compiler/src/cli/tsgen/utils.rs
+++ rust/compiler/src/cli/tsgen/utils.rs
@@ -1,4 +1,3 @@
-
 pub fn rel_import(src: &String, dst: &String) -> String {
     let src_v: Vec<&str> = src.split(['.']).collect();
     // strip off the name, only leave the path
@@ -15,8 +14,16 @@ pub fn rel_import(src: &String, dst: &String) -> String {
         }
         common += 1;
     }
+    // dbg!(&src_v);
+    // dbg!(common);
+    // dbg!(&dst_v);
     import.push_str("../".repeat(src_v.len() - common).as_str());
-    import.push_str(dst_v[common..].join("/").as_str());
+    if common == dst_v.len() {
+        import.push_str("../");
+        import.push_str(dst_v.join("/").as_str());
+    } else {
+        import.push_str(dst_v[common..].join("/").as_str());
+    }
     import
 }
 
@@ -26,7 +33,6 @@ mod tests {
 
     #[test]
     fn test_relative_import() {
-
         let tests: Vec<(&str, &str, &str, &str)> = vec![
             ("test 00", "abc", "runtime.adl", "./runtime/adl"),
             ("test 01", "scopedname.def", "scopedname.abc", "./abc"),
@@ -42,6 +48,9 @@ mod tests {
                 "runtime.adl",
                 "./../runtime/adl",
             ),
+            ("test 04", "common.adminui.api", "common", "./../../common"),
+            ("test 05", "common.adminui.api", "common.db", "./../db"),
+            ("test 06", "common.adminui.api", "common.adminui", "./../common/adminui"),
         ];
 
         for t in tests {
